9271633249781029c946577db965ccaffab7a0bd
Александр Малков <alexmal289@gmail.com>
2020-03-19 15:28:02 +0300

    qwe

--- main.cpp
+++ main.cpp
@@ -5,8 +5,6 @@
 
 int main() {
     auto logger = spdlog::stdout_logger_mt("console");
-//    std::cout << "Hello, World!" << std::endl;
     logger->info("version {} was started!", version());
-//    logger->error("Hello world!");
     return 0;
 }
